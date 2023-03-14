da36b32ab38864d89da0503d885ecf1ccc8bf564
James Feeney <dear.jameschan@gmail.com>
2023-03-14 14:24:14 -0700

    Initial Commit

---
+++ cs 32 project 4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cs 32 project 4
+//
+//  Created by James Chan Feeney on 3/14/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
